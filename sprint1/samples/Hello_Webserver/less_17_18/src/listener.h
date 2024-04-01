b24d64a3fa6c17f063437ffea2d9f40dc14bc25c
danil <dldanildm@mail.ru>
2024-04-01 16:21:52 +0300

    add async_server solution

---
+++ sprint1/samples/Hello_Webserver/less_17_18/src/listener.h
@@ -0,0 +1,8 @@
+//
+// Created by danil on 01.04.2024.
+//
+
+#ifndef HELLO_LISTENER_H
+#define HELLO_LISTENER_H
+
+#endif //HELLO_LISTENER_H
---
+++ sprint1/samples/Hello_Webserver/less_17_18/src/session.h
@@ -0,0 +1,8 @@
+//
+// Created by danil on 01.04.2024.
+//
+
+#ifndef HELLO_SESSION_H
+#define HELLO_SESSION_H
+
+#endif //HELLO_SESSION_H
