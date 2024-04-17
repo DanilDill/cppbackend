6adef4ecab348e22cd5a81f7c0e3fa942b52d3e1
danil <dldanildm@mail.ru>
2024-04-17 13:49:12 +0300

    solve server_logger

--- sprint2/problems/server_logging/solution/src/logging_request_handler.h
+++ sprint2/problems/server_logging/solution/src/logging_request_handler.h
@@ -1,17 +1,47 @@
 #pragma once
 #include <boost/log/trivial.hpp>
-#include <boost/json.hpp>
-#include <boost/log/core.hpp>
-#include <boost/date_time.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/attributes.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
 #include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/sources/logger.hpp>
+
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
+
+#include <boost/date_time.hpp>
+
+
+#include <boost/json.hpp>
+
 #include <queue>
 #include <ostream>
 #include <iostream>
+
+namespace logging = boost::log;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+namespace expr = boost::log::expressions;
+namespace attrs = boost::log::attributes;
+
+
+
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(line_id, "LineID", unsigned int)
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(data, "data", boost::json::value)
+BOOST_LOG_ATTRIBUTE_KEYWORD(message, "message", std::string)
+void Formatter(logging::record_view const& rec, logging::formatting_ostream& strm)
+{
+    strm << "{"<<"\"timestamp\":";
+    auto ts = rec[timestamp];
+    strm <<"\""<< to_iso_extended_string(*ts) << "\"" << ",";
+    strm << "\"data\":"<< rec[data] <<"," << "\"message\":"<<"\""<<rec[message] <<"\"";
+    strm << "}";
+}
+
 namespace http = boost::beast::http;
 template<typename Handler>
 class LoggingRequestHandler
@@ -24,17 +54,12 @@ public:
         InitLogger();
     };
 
-    void Log(boost::log::trivial::severity_level lvl,const boost::json::value& data, const std::string& message="")
+    void Log(boost::log::trivial::severity_level lvl,const boost::json::value& json_data, const std::string& message_str="")
     {
-        BOOST_LOG_SEV(logger, lvl) << data << "," << LogMessage(message);
+      //  BOOST_LOG_SEV(logger, lvl) << data << "," << LogMessage(message);
+        BOOST_LOG_TRIVIAL(info)  << logging::add_value(data, json_data) << logging::add_value(message,message_str);
     };
 
-    std::string LogMessage(const std::string& msg)
-    {
-       std::stringstream ss;
-       ss << "\"" << "message" << "\"" << ":"<< "\"" << msg << "\"";
-        return ss.str();
-    }
 
 
     void operator()(auto && req, auto&& send)
@@ -43,17 +68,13 @@ public:
         std::chrono::system_clock::time_point start_ts = std::chrono::system_clock::now();
         auto senderFuncttor = [this, &start_ts, &enpoint_str, sender = std::move(send)](auto&& response)
         {
-            /// я могу модифицировать обработчик отправки тут
             auto content_type = response[http::field::content_type];
             content_type = content_type == "" ? "null" : content_type;
-
             std::chrono::system_clock::time_point end_ts = std::chrono::system_clock::now();
-
-
             Log(boost::log::trivial::severity_level::info,{{"ip",enpoint_str},
                                                            {"response_time", (end_ts - start_ts)/1ms},
 
-                                                           {"code",response.result_int()},{"content_type",content_type}},"responce sent");
+                                                           {"code",response.result_int()},{"content_type",content_type}},"response sent");
             sender(response);
         };
         Log(boost::log::trivial::severity_level::info,{{"ip",enpoint_str},{"URI",req.first.target()},{"method",req.first.method_string()}},"request received");
@@ -65,22 +86,9 @@ private:
 
     void InitLogger()
     {
-        // Конфигурация формата вывода в JSON
         boost::log::add_common_attributes();
-        boost::log::core::get()->add_global_attribute("Scope", boost::log::attributes::named_scope());
-        boost::log::add_common_attributes();
-
-        // Установка формата вывода в JSON
-
-       boost::log::add_console_log(
-                std::cout,
-                boost::log::keywords::format = boost::log::expressions::format("{\"timestamp\":\"%1%\",\"data\":%2%}")
-                                               % boost::log::expressions::attr<boost::log::attributes::local_clock::value_type>("TimeStamp")
-                                               % boost::log::expressions::smessage
-        );
-
+        boost::log::add_console_log(std::clog, keywords::format = &Formatter);
     }
 private:
-    boost::log::sources::severity_logger<boost::log::trivial::severity_level> logger;
     Handler& decorated_handler_;
 };
