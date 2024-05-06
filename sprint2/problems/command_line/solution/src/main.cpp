02d3915264ee9f82aa6a3c4064f107b08b33b422
danil <dldanildm@mail.ru>
2024-05-06 16:52:44 +0300

    add commandline options

--- sprint2/problems/command_line/solution/src/main.cpp
+++ sprint2/problems/command_line/solution/src/main.cpp
@@ -1,5 +1,8 @@
 #include "sdk.h"
 //
+//#include <boost/program_options.hpp>
+#include <boost/program_options.hpp>
+#include <fstream>
 #include <boost/asio/signal_set.hpp>
 #include <boost/asio/io_context.hpp>
 #include <iostream>
@@ -11,6 +14,45 @@
 #include "staticfile_loader.h"
 using namespace std::literals;
 namespace net = boost::asio;
+struct Args {
+    std::string config_file;
+    std::string wwwroot;
+    std::optional<size_t> tick_period;
+    bool randomize_spawn_points;
+};
+
+[[nodiscard]] std::optional<Args> ParseCommandLine(int argc, const char* const argv[])
+{
+    namespace po = boost::program_options;
+    po::options_description desc{"All options"s};
+    Args args;
+    desc.add_options()
+    ("help,h", "Show help")
+    ("config-file,c", po::value(&args.config_file)->value_name("file"s), "set config file path")
+    ("www-root,w",po::value(&args.wwwroot)->value_name("dir"),"set static files root")
+    ("tick-period,t",po::value(&args.tick_period)->value_name("milliseconds"s),"set tick period")
+    ("randomize-spawn-points", po::value(&args.randomize_spawn_points),"spawn dogs at random positions ");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.contains("help"s)) {
+        // Если был указан параметр --help, то выводим справку и возвращаем nullopt
+        std::cout << desc;
+        return std::nullopt;
+    }
+    if (!vm.contains("config-file"))
+    {
+        throw std::runtime_error("config file was not found");
+    }
+    if (!vm.contains("www-root"))
+    {
+        throw std::runtime_error("wwwroot not specified");
+    }
+    return args;
+}
+
 
 namespace {
 
@@ -31,49 +73,41 @@ void RunWorkers(unsigned n, const Fn& fn) {
 
 int main(int argc, const char* argv[])
 {
-    if (argc != 3) {
-        std::cerr << "Usage: game_server <game-config-json> <wwwroot_dir>"sv << std::endl;
-        return EXIT_FAILURE;
-
-    }
     try {
-        // 1. Загружаем карту из файла и построить модель игры
-        const unsigned num_threads = std::thread::hardware_concurrency();
-        net::io_context ioc(num_threads);
-        model::Game game = json_loader::LoadGame(argv[1],ioc);
-        file::file_loader wwwroot(argv[2]);
-     // Создаём обработчик HTTP-запросов и связываем его с моделью игры
-        http_handler::RequestHandler handler{game,wwwroot};
-        LoggingRequestHandler<http_handler::RequestHandler> log_handler{handler};
-
-        net::signal_set signals(ioc, SIGINT, SIGTERM);
-        signals.async_wait([&ioc,&log_handler](const boost::system::error_code& ec, [[maybe_unused]] int signal_number) {
-            if (!ec) {
-                log_handler.Log(boost::log::trivial::info,{{"code",0}},"server exited");
-                ioc.stop();
-            }
-        });
-        // 3. Добавляем асинхронный обработчик сигналов SIGINT и SIGTERM
-
-
-        // 5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
-        const auto address = net::ip::make_address("0.0.0.0");
-        constexpr net::ip::port_type port = 8080;
-        http_server::ServeHttp(ioc, {address, port}, [&log_handler](auto&& req,auto&& send)
+        if(auto args = ParseCommandLine(argc,argv))
         {
-            log_handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
-        });
+            // 1. Загружаем карту из файла и построить модель игры
+            const unsigned num_threads = std::thread::hardware_concurrency();
+            net::io_context ioc(num_threads);
+            model::Game game = json_loader::LoadGame(argv[1],ioc);
+            file::file_loader wwwroot(argv[2]);
+            // Создаём обработчик HTTP-запросов и связываем его с моделью игры
+            http_handler::RequestHandler handler{game,wwwroot};
+            LoggingRequestHandler<http_handler::RequestHandler> log_handler{handler};
 
+            net::signal_set signals(ioc, SIGINT, SIGTERM);
+            signals.async_wait([&ioc,&log_handler](const boost::system::error_code& ec, [[maybe_unused]] int signal_number) {
+                if (!ec) {
+                    log_handler.Log(boost::log::trivial::info,{{"code",0}},"server exited");
+                    ioc.stop();
+                }
+            });
+            const auto address = net::ip::make_address("0.0.0.0");
+            constexpr net::ip::port_type port = 8080;
+            http_server::ServeHttp(ioc, {address, port}, [&log_handler](auto&& req,auto&& send)
+            {
+                log_handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
+            });
 
-
-        // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
-       // std::cout << "Server has started..."sv << std::endl;
-       log_handler.Log(boost::log::trivial::info,{{"port",port},{"address",address.to_string()},},"server started");
-
-        // 6. Запускаем обработку асинхронных операций
-        RunWorkers(std::max(1u, num_threads), [&ioc] {
-            ioc.run();
-        });
+            log_handler.Log(boost::log::trivial::info,{{"port",port},{"address",address.to_string()},},"server started");
+            RunWorkers(std::max(1u, num_threads), [&ioc] {
+                ioc.run();
+            });
+        }
+        else
+        {
+            return 0;
+        }
     } catch (const std::exception& ex) {
 
         std::cerr << ex.what() << std::endl;
