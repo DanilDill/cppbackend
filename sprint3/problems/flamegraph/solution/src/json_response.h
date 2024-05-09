4ce75e072b658a94f74369af3c1be32bf7591459
danil <dldanildm@mail.ru>
2024-05-09 19:16:27 +0300

    solve flamegraph

---
+++ sprint3/problems/flamegraph/solution/src/json_response.h
@@ -0,0 +1,9 @@
+#include <string>
+#include "model.h"
+namespace json_responce
+{
+    std::string to_json(const std::vector<model::Map>& maps);
+    std::string to_json(const model::Map& map);
+    std::string NotFoundJson();
+    std::string BadRequestJson();
+}
---
+++ sprint3/problems/flamegraph/solution/src/request_handler.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "http_server.h"
+#include "model.h"
+
+
+namespace http_handler {
+namespace beast = boost::beast;
+namespace http = beast::http;
+using namespace std::literals;
+using StringResponse = http::response<http::string_body>;
+using StringRequest = http::request<http::string_body>;
+
+    struct ContentType {
+        ContentType() = delete;
+        constexpr static std::string_view TEXT_HTML = "text/html"sv;
+        constexpr static std::string_view APPLICATION_JSON = "application/json"sv;
+
+    };
+class RequestHandler {
+public:
+    explicit RequestHandler(model::Game& game)
+        : game_{game} {
+    }
+
+    RequestHandler(const RequestHandler&) = delete;
+    RequestHandler& operator=(const RequestHandler&) = delete;
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send)
+    {
+      send(HandleRequest(std::forward<decltype(req)>(req)));
+    }
+
+
+    StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
+                                      bool keep_alive,
+                                      std::string_view content_type = ContentType::TEXT_HTML);
+
+    StringResponse HandleHead(StringRequest&& req);
+
+    StringResponse HandleGetMaps(StringRequest&& req);
+    StringResponse HandleGetMap(const std::string& map_id, StringRequest&& req);
+    StringResponse HandleBadRequest(StringRequest&& req);
+    StringResponse HandleGet(StringRequest&& req);
+    StringResponse HandleUnexpected(StringRequest&& req);
+    StringResponse HandleRequest(StringRequest&& req);
+
+
+private:
+    model::Game& game_;
+};
+
+}  // namespace http_handler
